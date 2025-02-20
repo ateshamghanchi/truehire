664c605720b30c9172b72e63510766de6079abd9
lightspeed-v8 <144581971+lightspeed-v8@users.noreply.github.com>
2025-02-20 19:14:58 +0530

    Apply Jobs Page Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import ApplyJob from './pages/ApplyJob.jsx'
 import Applications from './pages/Applications.jsx'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         <Route path='/apply-job/:id' element={<ApplyJob />} />
         <Route path='/applications' element={<Applications />} />
       </Routes>
+      <Analytics />
+      <SpeedInsights />
     </div>
   )
 }

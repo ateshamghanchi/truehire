ac0c654b2e8988e0c01e63a86a2f3fd26ad7d455
Jay Mevada <jkmevada.2003@gmail.com>
2025-02-20 13:45:06 +0530

    Home page completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Hero from '../components/Hero.jsx'
+import JobListing from '../components/JobListing.jsx'
+import Footer from '../components/Footer.jsx'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+      <Hero />
+      <JobListing />
+      <Footer />
+    </div>
   )
 }
 
